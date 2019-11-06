ed166d7014849174f1f2f41019b42dd56fe73efa
just-pie <stefi7773@gmail.com>
2019-11-06 21:38:07 +0100

    Skúška branchu

--- app/Models/Kontakty.php
+++ app/Models/Kontakty.php
@@ -8,6 +8,8 @@ class Kontakty extends Model
 {
     protected $table = 'kontakty';
 
+
+
     protected $primaryKey = 'id';
 
     protected $fillable = ['meno', 'email', 'telefon', 'pozicia', 'typ', 'katedrovy', 'fakulta', 'poznamka'];
