b8f43cf45d20c3d7c482ff86305655dd48179adf
just-pie <stefi7773@gmail.com>
2019-11-12 21:09:58 +0100

    Admin rozhranie

--- resources/views/admin/admin.blade.php
+++ resources/views/admin/admin.blade.php
@@ -1,1 +1,11 @@
-<?php
+<!DOCTYPE html>
+<html lang="sk">
+@include('admin_head')
+<body>
+<div class="page-wrapper default-theme sidebar-bg bg1 toggled">
+@include('admin_sidebar')
+@include('admin_body')
+</div>
+@include('admin_footer')
+</body>
+</html>
